72440118e98753362b7162bf0d550138b6d87b2c
Arnaldo Pacheco S <arnaldopachecosarmiento@gmail.com>
2023-09-22 02:13:56 -0500

    Add files via upload

--- Index.php
+++ Index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
-    <title>Quién quiere ser millonario</title>
+    <title>>HR mastermind</title>
 </head>
 <body>
     <div class="container">
